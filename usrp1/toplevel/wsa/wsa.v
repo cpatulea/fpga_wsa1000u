64f972fb2c52da558f84693f9db25f6c3d01681d
Catalin Patulea <cat@vv.carleton.ca>
2010-04-21 00:50:33 -0400

    Hook up red board PLL (ADF4350)

--- usrp1/toplevel/wsa/wsa.v
+++ usrp1/toplevel/wsa/wsa.v
@@ -150,6 +150,11 @@ module wsa(
    assign SDIO = USB_PA6_SDI; // never read from ADC
    assign ADC_CS_N = serial_csn[1]; // active low
 
+   // SPI bus to Patrick's ADF4350 board.
+   assign PROTO_PORT[0] = USB_PA4_SCLK;
+   assign PROTO_PORT[2] = USB_PA6_SDI;
+   assign PROTO_PORT[4] = serial_csn[2];
+
    // TODO: Hang these off a SPI register.
    assign VCO_LE = 0;
    // VCO_MUXOUT
